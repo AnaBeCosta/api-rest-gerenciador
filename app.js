e128568d3b17a5f78b2209e3bb27f18aa9b94c5b
AnaBeCosta <anacost.a@outlook.com>
2023-06-06 23:19:00 -0300

    Adicionando Swagger

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
-const routes = require("./routes/route");
+const routes = require("./routes/routes");
 const connectToDatabase = require('./database/connect');
+const swaggerUI = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
+app.use("/api-docs",swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 connectToDatabase();
 
